ff6ad0cf8576a9834e667c62d46267cb784f9b17
Arve Knudsen <arve.knudsen@gmail.com>
2021-09-13 15:58:12 +0200

    Make gRPC ping time and timeout into parameters
    Signed-off-by: Arve Knudsen <arve.knudsen@gmail.com>

--- crypto/tls/tls.go
+++ crypto/tls/tls.go
@@ -72,10 +72,17 @@ func (cfg *ClientConfig) GetTLSConfig() (*tls.Config, error) {
 	return config, nil
 }
 
-// GetGRPCDialOptions creates GRPC DialOptions for TLS
+// WithInsecure wraps grpc.WithInsecure.
+//
+// Stubbable for tests.
+var WithInsecure = func() grpc.DialOption {
+	return grpc.WithInsecure()
+}
+
+// GetGRPCDialOptions creates GRPC DialOptions for TLS.
 func (cfg *ClientConfig) GetGRPCDialOptions(enabled bool) ([]grpc.DialOption, error) {
 	if !enabled {
-		return []grpc.DialOption{grpc.WithInsecure()}, nil
+		return []grpc.DialOption{WithInsecure()}, nil
 	}
 
 	tlsConfig, err := cfg.GetTLSConfig()
